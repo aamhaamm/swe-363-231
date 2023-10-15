dbec6e403de779526bce8228228fb8f156f47223
Abdullah Ali <abdullaham1422@gmail.com>
2023-10-16 02:45:27 +0300

    JS_3-Exercise_1

---
+++ Weather.js
@@ -0,0 +1,57 @@
+const API_KEY = '72669bc34bcf456c89b80dc264a9f234';
+
+function fetchCitySuggestions() {
+    const query = document.getElementById('cityInput').value;
+    if (query.length < 3) {
+        document.getElementById('suggestions').style.display = 'none';
+        return;
+    }
+
+    fetch(`https://api.teleport.org/api/cities/?search=${query}`)
+    .then(response => response.json())
+    .then(data => {
+        const suggestionsBox = document.getElementById('suggestions');
+        suggestionsBox.innerHTML = '';
+        data._embedded['city:search-results'].forEach(city => {
+            const p = document.createElement('p');
+            p.textContent = city.matching_full_name;
+            p.onclick = function() {
+                document.getElementById('cityInput').value = city.matching_full_name.split(',')[0];
+                suggestionsBox.style.display = 'none';
+            };
+            suggestionsBox.appendChild(p);
+        });
+        suggestionsBox.style.display = 'block';
+    });
+}
+
+function getWeatherByInput() {
+    const cityName = document.getElementById('cityInput').value.trim();
+    if (!cityName) {
+        document.getElementById('weatherInfo').innerHTML = "Please enter a city name.";
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.cod && data.cod !== 200) {
+            throw new Error(data.message);
+        }
+        const weatherInfo = `
+            <h2>${data.name}</h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+        `;
+        document.getElementById('weatherInfo').innerHTML = weatherInfo;
+    })
+    .catch(error => {
+        console.error("Error fetching weather data:", error);
+        document.getElementById('weatherInfo').innerHTML = error.message;
+    });
+}
