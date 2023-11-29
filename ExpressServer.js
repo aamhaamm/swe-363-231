4908d73586f3ccf4abdfd672568fef2d843fb4fd
Abdullah Ali <abdullaham1422@gmail.com>
2023-11-29 09:52:48 +0300

    BE 3 Exercise 4

--- ExpressServer.js
+++ ExpressServer.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Routers
 const mainRouter = require("./mainRouter");
 const contactRouter = require("./contactRouter");
 const experienceRouter = require("./experienceRouter");
 
-app.use(express.static("./"));
+// Middleware to handle form submissions with URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to handle the form submission
+app.post('/submit-form', (req, res) => {
+  // req.body will contain the form data from the request
+  console.log(req.body);
+
+  // Process the form data here
+  // ...
+
+  // Send a response to the client
+  res.send('Form submission processed.');
+});
 
 // Use the routers
 app.use("/", mainRouter);
